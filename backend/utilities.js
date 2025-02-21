169884ee174ccff8927af3ea876d518e3e355ae8
unknown <zxckendrick123@gmail.com>
2025-02-21 16:29:38 +0800

    Fixed response in jwt authentication

--- backend/utilities.js
+++ backend/utilities.js
@@ -2,13 +2,20 @@ import jwt from 'jsonwebtoken'
 
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(" ") [1];
+  const accessToken = authHeader && authHeader.split(" ") [1];
 
-  if(!token) return res.sendStatus(401);
+  if(!accessToken) {
+    return res.status(401).json({ error: "Access denied. No token provided." });
+  };
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=> {
-    if(err) return res.sendStatus(401);
-    req.user = user;
-    next();
- });
+  try{
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user)=> {
+      if(err) return res.sendStatus(401);
+      req.user = user;
+      next();
+    })
+  } catch (err) {
+    res.status(403).json({ error: "Invalid or expired token" });
+  }
+
 }
