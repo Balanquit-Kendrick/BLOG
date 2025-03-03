0b2089056568559f9b13f0a1b1d99eb0ece1586b
unknown <zxckendrick123@gmail.com>
2025-03-03 21:12:07 +0800

    Fixed right sidebar

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = () => {
   return (
     <div className="flex-1 overflow-y-auto bg-gray-100">
-      <div className="h-[200vh] bg-blue-200 flex justify-center items-center content-center">Long content here</div>
+      <div className="h-[200vh] flex justify-center items-center content-center">Long content here</div>
     </div>
   )
 }
--- frontend/src/components/right-sidebar.jsx
+++ frontend/src/components/right-sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSideBar = () => {
   return (
-    <div className="w-[250px] bg-secondary h-[calc(100vh-48px)] fixed right-0 top-[48px] flex items-center justify-center">
+    <div className="w-[250px] bg-sidebar h-[calc(100vh-48px)] fixed right-0 top-[48px] flex items-center justify-center">
       Right Sidebar
     </div>
   )
