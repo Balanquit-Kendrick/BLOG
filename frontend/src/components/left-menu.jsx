d1bdea4fcc5ad15fd6f90d0446eaa7d8e41b45a4
unknown <zxckendrick123@gmail.com>
2025-03-04 21:18:19 +0800

    Added menu items

--- frontend/src/components/left-menu.jsx
+++ frontend/src/components/left-menu.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from './ui/sidebar'
+import { ChevronRight } from 'lucide-react'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@radix-ui/react-collapsible'
 
-const LeftMenu = () => {
+export function LeftMenu({
+  items
+})
+
+{
   return (
-    <div>
-        
-    </div>
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible key={item.title} asChild defaultOpen={item.isActive} className="group/collapsible">
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {item.items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                      <SidebarMenuSubButton asChild>
+                        <a href={subItem.url}>
+                          <span>{subItem.title}</span>
+                        </a>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
   )
 }
-
-export default LeftMenu
--- frontend/src/components/left-sidebar.jsx
+++ frontend/src/components/left-sidebar.jsx
@@ -1,11 +1,117 @@
 import { getUserInfo } from '@/store/auth';
 import React, { useEffect, useState } from 'react'
 import { ProfileDropDown } from './profile-drop';
-import LeftMenu from './left-menu';
+import { LeftMenu } from './left-menu';
 import { Sidebar, SidebarContent, SidebarHeader, SidebarProvider, SidebarRail } from './ui/sidebar';
+import { BookOpen, Bot, Frame, PieChart, Settings2, SquareTerminal } from 'lucide-react';
 
 const LeftSideBar = ({...props}) => {
-
+  const data = {
+    navMain: [
+      {
+        title: "Playground",
+        url: "#",
+        icon: SquareTerminal,
+        isActive: true,
+        items: [
+          {
+            title: "History",
+            url: "#",
+          },
+          {
+            title: "Starred",
+            url: "#",
+          },
+          {
+            title: "Settings",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Models",
+        url: "#",
+        icon: Bot,
+        items: [
+          {
+            title: "Genesis",
+            url: "#",
+          },
+          {
+            title: "Explorer",
+            url: "#",
+          },
+          {
+            title: "Quantum",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Documentation",
+        url: "#",
+        icon: BookOpen,
+        items: [
+          {
+            title: "Introduction",
+            url: "#",
+          },
+          {
+            title: "Get Started",
+            url: "#",
+          },
+          {
+            title: "Tutorials",
+            url: "#",
+          },
+          {
+            title: "Changelog",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Settings",
+        url: "#",
+        icon: Settings2,
+        items: [
+          {
+            title: "General",
+            url: "#",
+          },
+          {
+            title: "Team",
+            url: "#",
+          },
+          {
+            title: "Billing",
+            url: "#",
+          },
+          {
+            title: "Limits",
+            url: "#",
+          },
+        ],
+      },
+    ],
+    projects: [
+      {
+        name: "Design Engineering",
+        url: "#",
+        icon: Frame,
+      },
+      {
+        name: "Sales & Marketing",
+        url: "#",
+        icon: PieChart,
+      },
+      {
+        name: "Travel",
+        url: "#",
+        icon: Map,
+      },
+    ],
+  }
   return (
     <div className='w-[250px] h-[calc(100vh-48px)] fixed left-0 top-[48px] flex items-center justify-center'>
       <SidebarProvider>
@@ -14,9 +120,7 @@ const LeftSideBar = ({...props}) => {
             <ProfileDropDown sidebar={true} />
           </SidebarHeader>
           <SidebarContent>
-            <LeftMenu />
-            {/* <NavMain items={data.navMain} />
-            <NavProjects projects={data.projects} /> */}
+            <LeftMenu items={data.navMain}/>
           </SidebarContent>
           {/* <SidebarFooter>
             <NavUser user={data.user} />
