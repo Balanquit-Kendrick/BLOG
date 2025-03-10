87d497b6236badb202818813cc5a6b29785aa427
unknown <zxckendrick123@gmail.com>
2025-03-10 22:02:00 +0800

    Added main button for add post

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 const Main = () => {
   return (
     <div className="flex-1 overflow-y-auto bg-gray-100">
-      <div className="h-[200vh] flex justify-center items-center content-center">Long content here</div>
+      <div className="h-[200vh] flex justify-center items-center content-center">
+        <div>
+          <Button>Add Post</Button>
+        </div>
+      </div>
     </div>
   )
 }
